eb25688822f86c1de4510074446dd8daf764955e
1 <1@121.179.35.101>
2022-02-25 17:17:08 +0900

    오류수정

--- kims/src/main/java/kims/model/UserVO.java
+++ kims/src/main/java/kims/model/UserVO.java
@@ -1,31 +1,29 @@
 package kims.model;
 
 public class UserVO {
-	private String id ;
-	   private String name;
-	   private String pw;
-	   private String tel;
-	   private String email;
-	   private String admin;
-	   
-<<<<<<< HEAD
-		public UserVO(String id, String pw) {
-			super();
-=======
-	   public UserVO(String id, String pw, String tel, String email) {
-			super();
-			this.id = id;
-			this.pw = pw;
-			this.tel = tel;
-			this.email = email;
-		}
-	   public UserVO(String id, String pw) {
->>>>>>> branch 'master' of https://github.com/2022-SMHRD-KDT-BigData-2/kims-project.git
-			this.id = id;
-			this.pw = pw;
-		}
+	private String id;
+	private String name;
+	private String pw;
+	private String tel;
+	private String email;
+	private String admin;
+	private String answer;
 
-	public UserVO(String id, String name, String pw, String tel, String email, String admin) {
+
+	public UserVO(String id, String pw, String tel, String email) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.tel = tel;
+		this.email = email;
+	}
+
+	public UserVO(String id, String pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+
+	public UserVO(String id, String name, String pw, String tel, String email, String admin, String answer) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -33,43 +31,55 @@ public class UserVO {
 		this.tel = tel;
 		this.email = email;
 		this.admin = admin;
+		this.answer=answer;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPw() {
 		return pw;
 	}
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
 	public String getTel() {
 		return tel;
 	}
+
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getAdmin() {
 		return admin;
 	}
+
 	public void setAdmin(String admin) {
 		this.admin = admin;
 	}
-	  
 
 }
